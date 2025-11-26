2dc06758d2fd0454c6c439cff7b228db47a743f1
kirillHvesyukovich <krillx94@gmail.com>
2025-11-26 14:53:47 +0100

    deleted unused filed

--- src/ui/pages/products/delete.modal.ts
+++ src/ui/pages/products/delete.modal.ts
@@ -4,7 +4,6 @@ import { SalesPortalPage } from "ui/pages/salesPortal.page.js";
 
 export class DeleteModal extends SalesPortalPage {
     readonly uniqueElement = this.page.locator("//div[@name ='confirmation-modal']");
-    readonly title = this.uniqueElement.locator('h5');
     readonly confirmDeteteButton = this.uniqueElement.locator("//button[@type = 'submit']");
     async confirmDetete(){
         this.confirmDeteteButton.click();
